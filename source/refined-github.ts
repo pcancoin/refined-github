1c9c052e5e367583ea5382953f392a954a4edc7f
Pablo C <pablo.cancoin@skyvisor.ai>
2025-12-09 10:40:06 +0100

    `pr-review-status` - Add feature

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -200,6 +200,7 @@ import './features/prevent-comment-loss.js';
 import './features/fix-no-pr-search.js';
 import './features/clean-readme-url.js';
 import './features/pr-notification-link.js';
+import './features/pr-review-status.js';
 import './features/click-outside-modal.js';
 import './features/hidden-content.js';
 import './features/linkify-line-numbers.js';
